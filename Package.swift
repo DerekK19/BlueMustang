19356e8224453d60979324d8c51625a52db0d557
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2020-07-18 17:45:25 +1200

    Added logging class

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "BlueMustang",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "BlueMustangTests",
             dependencies: ["BlueMustang"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BlueMustang
+============
+[![Swift Package Manager](https://rawgit.com/jlyonsmith/artwork/master/SwiftPackageManager/swiftpackagemanager-compatible.svg)](https://swift.org/package-manager/)
 
-A description of this package.
+BlueMustang is a framework written in Swift that intefaces to a Fender Mustang Amplifier through a Bluetooth bridge application
