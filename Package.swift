f24ef2f71342d9b4f5aaa35cfb6cda73c3eeba1b
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2020-10-22 17:15:03 +1300

    Use 4.6.0 version of AEXML

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.5.0"))
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.6.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
